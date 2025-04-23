e3257871e2cd9349b8855767bb522f006bb8b331
jefflds <jeffersonlds3009@gmail.com>
2025-04-22 21:26:40 -0300

    fix: sidebar sales link

--- src/view/layout/AdminLayout.tsx
+++ src/view/layout/AdminLayout.tsx
@@ -6,15 +6,20 @@ import Header from './Header/Header';
 import { Sidebar, SidebarItems } from './Sidebar/Sidebar';
 
 const menuItems: SidebarItems = [
-  { type: 'link', icon: Tags, label: 'Cupons', path: '/coupons' },
-  { type: 'link', icon: CreditCard, label: 'Vendas', path: '/sales' },
+  { type: 'link', icon: Tags, label: 'Cupons', path: ROUTES.coupons },
+  { type: 'link', icon: CreditCard, label: 'Vendas', path: ROUTES.sales.home },
   {
     type: 'link',
     icon: UserX,
     label: 'Usuários Bloqueados',
-    path: ROUTES.users.blocked,
+    path: ROUTES.users.blocked.home,
   },
-  { type: 'link', icon: Settings, label: 'Configurações', path: '/settings' },
+  {
+    type: 'link',
+    icon: Settings,
+    label: 'Configurações',
+    path: ROUTES.settings,
+  },
 ];
 
 export function AdminLayout() {
