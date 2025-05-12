724fef41ef55be15d190d895c91418f1af60977d
Lucas M. Campos <lucasm.campos@hotmail.com.br>
2025-05-12 08:43:31 -0300

    fix(model): handle null values in ListAllBlockedUserModel fields

--- src/data/model/user/user.model.ts
+++ src/data/model/user/user.model.ts
@@ -49,6 +49,10 @@ export const ListAllBlockedUserModel = z.object({
         transactionDate: z.string(),
         cupom: z.string(),
         amount: z.number(),
+        valorBRL: z
+          .number()
+          .nullable()
+          .transform((val) => val ?? 0), // Temporário
         cryptoValue: z.number().optional(),
         cryptoType: CryptoTypeModel,
         status: PaymentStatusModel,
@@ -59,9 +63,21 @@ export const ListAllBlockedUserModel = z.object({
             z
               .object({
                 id: z.number(),
-                pegId: z.string(),
-                LiquidTxId: z.string(),
-                MempoolTxId: z.string(),
+                pegId: z
+                  .string()
+                  .optional()
+                  .nullable()
+                  .transform((val) => val ?? ''),
+                LiquidTxId: z
+                  .string()
+                  .optional()
+                  .nullable()
+                  .transform((val) => val ?? ''),
+                MempoolTxId: z
+                  .string()
+                  .optional()
+                  .nullable()
+                  .transform((val) => val ?? ''),
               })
               .optional(),
           )
