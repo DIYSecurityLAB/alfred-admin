8b285129a72a1f36bcace692105f11831b3bd6be
jefflds <jeffersonlds3009@gmail.com>
2025-05-31 03:00:02 -0300

    feat(csrf): add CSRF token to request headers in RemoteDataSource classes

--- src/data/datasource/RemoteOld.datasource.ts
+++ src/data/datasource/RemoteOld.datasource.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { getCsrfToken } from '@/utils/Csrf';
 import axios, {
   AxiosError,
   AxiosInstance,
@@ -50,7 +51,18 @@ export class RemoteDataSourceOld {
       headers: {
         'x-api-key': import.meta.env.VITE_API_KEY, // TODO: Use HeaderValues
       },
+      withCredentials: true,
     });
+
+    this.api.interceptors.request.use(
+      (config) => {
+        config.headers['X-CSRF-Token'] = getCsrfToken();
+        return config;
+      },
+      (error) => {
+        return Promise.reject(error);
+      },
+    );
   }
 
   public setBaseURL(baseURL: string): void {
