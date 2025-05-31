92889faec960bfb235b239ba4ad2c26b731bb6d3
jefflds <jeffersonlds3009@gmail.com>
2025-05-31 02:59:06 -0300

    feat(csrf): add function to retrieve CSRF token from cookies

---
+++ src/utils/Csrf.ts
@@ -0,0 +1,13 @@
+export function getCsrfToken() {
+  const name = '_csrf=';
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(';');
+
+  for (let i = 0; i < ca.length; i++) {
+    const c = ca[i].trim();
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length);
+    }
+  }
+  return '';
+}
